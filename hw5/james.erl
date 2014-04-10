a9c923b56a5ead12e3037198487fe4cc47de0795
TheFuzz1991 <jamesreinke91@gmail.com>
2014-04-10 11:12:03 -0700

    finally finished james

--- hw5/james.erl
+++ hw5/james.erl
@@ -107,11 +107,12 @@ storage_process(Dictionary, Process_Number, M)->
 
 		% list of node numbers currently in the system
 		{PID, Ref, node_list}->
-			nothing;
+			Result = get_node_list(0, M, []),
+			PID ! {Ref, Result, result};
 
 		% sent from the controller to leave the system
 		{PID, Ref, leave}->
-			nothing;
+			ok;
 
 		% send the dictionary to the collector
 		{PID, Ref, Collector_Number, snapshot} ->
@@ -151,10 +152,6 @@ hash(String, Total, M) ->
 get_value([], Key) -> no_value;
 
 get_value(Dictionary, Key) -> no_value.
-
-get_smallest([]) -> no_value;
-
-get_smallest(Dictionary) -> 0. 
 
 % returns the node number that a storage process should send the message to for greatest efficiency
 % Start Node: the node sending the message
@@ -215,6 +212,22 @@ send_dictionary([], Collector_Number) ->
 send_dictionary(Dictionary, Collector_Number) ->
 	send({hd(Dictionary), dict_item}, Collector_Number, storage_process),
 	send_dictionary(tl(Dictionary), Collector_Number).
+
+get_node_list(Index, M, Node_List) ->
+	Node_Number = get_node(Index, M),
+	Total = round(math:pow(2, M) - 1),
+	if 
+		Index == Node_Number ->
+			% add the node to the list
+			get_node_list(Index + 1, M, Node_List++[Index]);
+		% we reached the end
+		Index >= Total ->
+			ok;
+		true -> 
+			get_node_list(Index + 1, M, Node_List)
+	end.
+
+
 
 
 
