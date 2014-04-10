d65ee4db11ac6ed4f400b6c1cd8897ef7fd1f29f
TheFuzz1991 <jamesreinke91@gmail.com>
2014-04-10 10:18:42 -0700

    updated james.erl

--- hw5/james.erl
+++ hw5/james.erl
@@ -14,54 +14,76 @@
 -export([main/0]).
 
 main() ->
-	Node1 = 35,
-	global:register_name(list_to_atom("Node5"), self()),
-	global:register_name(list_to_atom("Node20"), self()),
-	global:register_name(list_to_atom("Node30"), self()),
-	global:register_name(list_to_atom("Node35"), self()),
-	Backup = backup_node(Node1,6),
-	io:format("Backup for ~p is ~p~n",[Node1, Backup]).
-
+	Return_Value = hash("Another God Damn Test", 0, 6),
+	io:format("Hash Value ~p~n", [Return_Value]).
 % storage process
 % List -- list of tuples; our dictionary
 % Number -- the process number
 % Node -- node address the process is currently on
 % M -- our constant value m
 % global:whereis_name(Name) will return the PID that we need to send a message to
-storage_process(List, Process_Number, M)-> 
+storage_process(Dictionary, Process_Number, M)-> 
 	
 	receive
 
 		% store a value for our key, msg the controller,
 		{PID, Ref, store, Key, Value}->
-			Send_To = hash(M, Key),
+			Send_To = hash(Key, 0, M),
 			if 
 				Send_To == Process_Number->
 					% send the old value to the controller
-					Old_Value = find_value_for_key(List, Key),
+					Old_Value = get_value(Dictionary, Key),
 					PID ! {Ref, stored, Old_Value},
 					% find our backup node and send a message to that process to backup our data
-					Recipient_ID = recipeint()
+					% finds intermediate node to pass message to if we can't make it in one hop
+					Process_ID = recipient(Process_Number, get_backup_node(Process_Number, M), 0, M),
+					Msg = {self(), make_ref(), backup, Key, Value},
+					send(Msg, Process_ID, storage_process),
 					% update the storage process
-					storage_process(List--[{Key, Old_Value}]++[{Key, Value}], Process_Number, M);
+					storage_process(Dictionary--[{Key, Old_Value}]++[{Key, Value}], Process_Number, M);
 
 				% the storage process does not have the key
 				true ->
 					% our next process which receives a message
-					Recipient_ID = recipient(Process_Number, Send_To, M),
-					% find the PID of our recipient
-					Recipient_PID = get_pid(Recipient_ID, storage_process),
-					% send the message along
-					Recipient_PID ! {PID, Ref, store, Key, Value}
+					Process_ID = recipient(Process_Number, Send_To, 0, M),
+					Msg = {PID, Ref, store, Key, Value},
+					send(Msg, Process_ID, storage_process)
+			end;
+		% a dictionary value meant for backup on the correct node
+		{PID, Ref, backup, Key, Value}->
+			Send_To = hash(Key, 0, M),
+			Node_ID = get_node(Process_Number, M),
+			Msg = {self(), Ref, backup, Key, Value},
+			if  % this process is in the same node so we send to our non-storage process
+				Send_To == Node_ID ->
+					send(Msg, Node_ID, node);
+				% the backup node is not on the node of the process so we forward
+				true ->
+					Process_ID = recipient(Process_Number, Send_To, 0, M),
+					send(Msg, Process_ID, storage_proess)
 			end;
 
 		% retrieve the value of a given key
-		{PID, Ref, retrieve, Key}->
-			nothing;
+		{PID, Ref, retrieve, Key}-> 
+			Send_To = hash(Key, 0, M),
+			if  % the key we want to retrieve is on this process
+				Send_To == Process_Number ->
+					Return_Value = get_value(Dictionary, Key),
+					PID ! {Ref, Return_Value, result};
+				true ->
+					% the key is not on this process
+					Process_ID = recipient(Process_Number, Send_To, 0, M),
+					send({PID, Ref, retrieve, Key}, Process_ID, storage_process)
+			end;
+
 
 		% find the last key in lexicographic order
 		{PID, Ref, first_key}->
-			nothing;
+			if
+				Process_Number == 0 ->
+					Return_Value = get_smallest;
+				true -> nothing
+			end;
 
 		% find the last key in lexicographic order
 		{PID, Ref, last_key}->
@@ -77,20 +99,37 @@ storage_process(List, Process_Number, M)->
 
 		% sent from the controller to leave the system
 		{PID, Ref, leave}->
-			nothing
+			nothing;
 
+		{PID, Ref, collect}->
+			% calculate the number of the last node in the system
+			Arith = round(math:pow(2, M) - 1),
+			if
+				Process_Number == Arith ->
+					nothing;
+				true ->
+					nothing
+			end
 	end.
 
+% return a mod of our hash value
+hash([], Total, M) -> 
+	Return = Total rem round(math:pow(2, M) - 1),
+	Return;
 
-hash([], Key) -> 0;
+% returns the hash value based on our global M
+hash(String, Total, M) ->
+	Arith = hd(String) + round(math:pow(2, 6)) + round(math:pow(2, 16)) - Total,
+	hash(tl(String), Arith + Total, M).
 
-% returns the hash value based on our global m
-hash(String, Key) -> 0.
 
+get_value([], Key) -> no_value;
 
-find_value_for_key([], Key) -> no_value;
+get_value(Dictionary, Key) -> no_value.
 
-find_value_for_key(Dictionary, Key) -> no_value.
+get_smallest([]) -> no_value;
+
+get_smallest(Dictionary) -> 0. 
 
 % returns the node number that a storage process should send the message to for greatest efficiency
 % Start Node: the node sending the message
@@ -115,29 +154,33 @@ recipient(Start_Node, End_Node, K, M) ->
 	end.
 
 
-% finds the index that the node belongs to
+% finds the index that the process belongs to
 get_node(Process_Number, M) ->
 	Arith = (Process_Number + round(math:pow(2,M))) rem round(math:pow(2,M)),
-	io:format("Arith String: ~p~n", [Arith_String]),
-	case get_pid(Arith, node) of
+	io:format("Arith String: ~p~n", [Arith]),
+	Name = list_to_atom("Node"++integer_to_list(Arith)),
+	case global:whereis_name(Name) of
 		undefined ->
-			backup_node(Process_Number - 1, M);
+			get_node(Process_Number - 1, M);
 		PID ->
 			Process_Number
 	end.
 
 % finds the node that is backing up a processes' information
 get_backup_node(Process_Number, M)->
-	Node_Number = get_node(Process_Number),
+	Node_Number = get_node(Process_Number, M),
 	Arith = (Node_Number - 1 + round(math:pow(2,M))) rem round(math:pow(2,M)),
-	get_node(Arith).
+	get_node(Arith, M).
 
-get_pid(Number, node)->
-	global:whereis_name(list_to_atom("Node"++integer_to_list(Number)));
 
-get_pid(Number, storage_process)->
-	global:whereis_name(list_to_atom("StorageProcess"++integer_to_list(Number))).
+% sends a message using our global table
+send(Msg, Number, node)->
+	Name = list_to_atom("Node"++integer_to_list(Number)),
+	global:send(Name, Msg);
 
+send(Msg, Number, storage_process)->
+	Name = list_to_atom("StorageProcess"++integer_to_list(Number)),
+	global:send(Name, Msg).
 
 
 
